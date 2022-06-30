45e8c1e3fe91f098fac3a23106c0d0c3274f219d
Maria Bernardino <contato@mariabernardino.com.br>
2022-06-30 15:06:27 -0300

    add nodemon

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const routerUser = require('./routes/user');
 const port = process.env.PORT || 8000;
+const cors = require('cors')
 
 app.use(cors());
 app.use(express.json())
