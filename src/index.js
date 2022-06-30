b754a8b92253957480223a857e2e730ff533481c
Maria Bernardino <contato@mariabernardino.com.br>
2022-06-30 14:18:24 -0300

    Fist commit users

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const routerUser = require('./routes/user');
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json())
+
+app.use(routerUser);
+
+app.listen(port, () =>{
+    console.log(`Server is Runnig at http://localhost:8000/`);
+})
