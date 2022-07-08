f626ac38252653c1781a634ca5ebef13c758a26c
Maria Bernardino <contato@mariabernardino.com.br>
2022-07-08 17:07:09 -0300

    Alterando as variaves de ambiente

--- src/controllers/user/login.js
+++ src/controllers/user/login.js
@@ -5,21 +5,20 @@ const hashPassword = require('../../utils/hash-password');
 const login = async (req, res) => {
   const { email } = req.body;
 
-  if (!email)
-    try {
-      const user = await knex('usuarios').where('email', email).first();
+  try {
+    const user = await knex('usuarios').where('email', email).first();
 
-      const token = jwt.sign({ id: user.id }, hashPassword, { expiresIn: '1d' });
+    const token = jwt.sign({ id: user.id }, hashPassword, { expiresIn: '1d' });
 
-      const { senha: _, ...userInformation } = user;
+    const { senha: _, ...userInformation } = user;
 
-      return res.status(200).json({
-        usuario: userInformation,
-        token,
-      });
-    } catch (error) {
-      return res.status(400).json(error.message);
-    }
+    return res.status(200).json({
+      usuario: userInformation,
+      token,
+    });
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
 };
 
 module.exports = {
