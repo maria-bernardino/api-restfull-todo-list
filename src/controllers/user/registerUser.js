b647e6c99a00ea1b85ee8f2cc03cc0067c17730d
Maria Bernardino <contato@mariabernardino.com.br>
2022-07-08 15:59:38 -0300

    Alterando as variaves de ambiente

--- src/controllers/user/registerUser.js
+++ src/controllers/user/registerUser.js
@@ -1,26 +1,26 @@
-const knex = require('../../db/conection')
-const bcrypt = require('bcrypt')
+const knex = require('../../db/conection');
+const bcrypt = require('bcrypt');
 
 async function signUp(req, res) {
-  const { nome, email, senha } = req.body
+  const { nome, email, senha } = req.body;
 
   if (!nome || !email || !senha) {
-    return res.status(400).json({ error: 'Todos os campos são obrigátorios!' })
+    return res.status(400).json({ error: 'Todos os campos são obrigátorios!' });
   }
 
   try {
-    const hash = await bcrypt.hash(senha, 10)
-    const user = await knex('usuarios').insert({ nome, email, senha: hash })
+    const hash = await bcrypt.hash(senha, 10);
+    const user = await knex('usuarios').insert({ nome, email, senha: hash });
 
     if (!user) {
-      return res.status(400).json({ error: 'Error ao realizar cadastro' })
+      return res.status(400).json({ error: 'Error ao realizar cadastro' });
     }
-    return res.status(201).json()
+    return res.status(201).json(user);
   } catch (error) {
-    return res.status(400).json(error.message)
+    return res.status(400).json(error.message);
   }
 }
 
 module.exports = {
   signUp,
-}
+};
