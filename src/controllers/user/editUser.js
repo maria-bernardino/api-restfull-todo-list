8f8d998ec2d50ba41e91e380e6d96166bf1ef256
Maria Bernardino <contato@mariabernardino.com.br>
2022-07-08 17:35:29 -0300

    Alterando as variaves de ambiente

--- src/controllers/user/editUser.js
+++ src/controllers/user/editUser.js
@@ -6,6 +6,7 @@ const editUser = async (req, res) => {
   const { usuario } = req;
 
   const verifyUser = await knex('usuarios').where('id', usuario.id).first();
+  const verifyEmail = await knex('usuarios').where('id', usuario.id).first();
 
   if (senha && senha !== verifyUser.senha) {
     const hash = await bcrypt.hash(senha, 10);
@@ -15,6 +16,14 @@ const editUser = async (req, res) => {
       .returning('*');
 
     res.status(200).json(userEdit[0]);
+  }
+  if (email && email !== verifyEmail) {
+    const hash = await bcrypt.hash(senha, 10);
+    const editEmail = await knex('usuarios')
+      .update({ nome, email, senha: hash })
+      .where('id', usuario.id)
+      .returning('*');
+    res.status(200).json(editEmail[0]);
   } else {
     const userEdit = await knex('usuarios')
       .update({ nome, email, senha: hash })
